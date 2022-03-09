ddbc9e66c7479116796b033a3b4ff5981044669f
Jackson Naufal <Jackson.naufal@mths.ca>
2022-03-09 21:25:17 +0000

    FixedLinterErrors

--- global_variables.py
+++ global_variables.py
@@ -4,30 +4,38 @@
 #  This is a "Global Variable Program"  with proper style"
 variable_X = 25
 
+
 def local_variable():
-# this shows what happens when you use local variables
+    # this shows what happens when you use local variables
     variable_X = 10
     variable_Y = 30
     variable_Z = variable_X + variable_Y
-    print("Local variable_X, variable_Y, variable_Z: {0} + {1} = {2}".
-        format(variable_X, variable_Y, variable_Z))
+    print(
+        "Local variable_X, variable_Y, variable_Z: {0} + {1} = {2}".format(
+            variable_X, variable_Y, variable_Z
+        )
+    )
+
 
 def global_variable():
-# this shows what happens when use use global variables
+    # this shows what happens when use use global variables
     global variable_X
     variable_X = variable_X + 1
     variable_Y = 30
     variable_Z = variable_X + variable_Y
 
+    print(
+        "Global variable_X, variable_Y, variable_Z: {0} + {1} = {2}".format(
+            variable_X, variable_Y, variable_Z
+        )
+    )
 
-    print("Global variable_X, variable_Y, variable_Z: {0} + {1} = {2}".
-        format(variable_X, variable_Y, variable_Z))
 
 def main():
     # this function shows how both local and global variables are
     local_variable()
     global_variable()
 
+
 if __name__ == "__main__":
     main()
-    
